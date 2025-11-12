32882a2ac46f89791c3dbc2a54f70274fb2419f1
AdamNafea-STUDENT <g00462155@atu.ie>
2025-11-12 17:32:27 +0000

    added multiplication and division exception handling

--- src/main/java/ie/atu/week8/Calculator.java
+++ src/main/java/ie/atu/week8/Calculator.java
@@ -15,5 +15,25 @@ public class Calculator {
         return firstNum - secondNum;
     }
 
+    public int divide(int i, int i1)
+    {
+        if(i1 == 0)
+            throw new ArithmeticException("Divide by zero");
+        return i / i1;
+    }
+
+    public int multiply(int i, int i1)
+    {
+        long result = (long) i * (long) i1;
+        if (result > Integer.MAX_VALUE) {
+            throw new ArithmeticException("Integer Overflow");
+        }
+        if (result < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Integer Underflow");
+        }
+        return (int) result;
+
+    }
+
 
 }
